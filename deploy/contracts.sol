c85f96e3fdd2b683a05e39484916b35b685afd29
cromlehg <cromlehg@gmail.com>
2018-01-13 13:50:33 +0300

    Update contracts.sol

--- deploy/contracts.sol
+++ deploy/contracts.sol
@@ -610,7 +610,7 @@ contract Configurator is Ownable {
   Mainsale public mainsale;
 
   function deploy() public onlyOwner {
-    owner = 0x95EA6A4ec9F80436854702e5F05d238f27166A03;
+    //owner = 0x95EA6A4ec9F80436854702e5F05d238f27166A03;
 
     token = new VestarinToken();
 
@@ -620,7 +620,7 @@ contract Configurator is Ownable {
     presale.addStage(5000,3000);
     presale.setMasterWallet(0x95EA6A4ec9F80436854702e5F05d238f27166A03);
     presale.setSlaveWallet(0x070EcC35a3212D76ad443d529216a452eAA35E3D);
-    presale.setSlaveWalletPercent(30);
+    presale.setSlaveWalletPercent(50);
     presale.setStart(1517317200);
     presale.setPeriod(30);
     presale.setMinPrice(100000000000000000);
@@ -637,7 +637,7 @@ contract Configurator is Ownable {
     mainsale.addStage(40000,1300);
     mainsale.setMasterWallet(0x95EA6A4ec9F80436854702e5F05d238f27166A03);
     mainsale.setSlaveWallet(0x070EcC35a3212D76ad443d529216a452eAA35E3D);
-    mainsale.setSlaveWalletPercent(30);
+    mainsale.setSlaveWalletPercent(50);
     mainsale.setFoundersTokensWallet(0x95EA6A4ec9F80436854702e5F05d238f27166A03);
     mainsale.setBountyTokensWallet(0x95EA6A4ec9F80436854702e5F05d238f27166A03);
     mainsale.setStart(1525352400);
